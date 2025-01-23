f7d97aa4f43af36cc0cc254c44ae2bf98d0ee792
Shubhanshu-222 <bansodshubhanshu@gmail.com>
2025-01-23 07:14:23 +0530

    date today

--- First.java
+++ First.java
@@ -1,5 +1,6 @@
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Date today is 23rd January 2025");
     }
 }
